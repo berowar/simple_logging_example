62fb2bb24252073448e27a064b3f545a2a059b1c
Dmitry Puchkov <berowar@gmail.com>
2011-09-29 15:52:50 +0300

    init project add scaffold Product

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoggerExampale::Application.config.secret_token = '2ce2a6e1bdb00ba62199219e32ef8e56494c4e205f0c9bef3226cc0f6f5364e60c47b2763a2e5d6e43cc17358e3aadd81e3d1790ff372adeaf60ab99f7cee76e'
